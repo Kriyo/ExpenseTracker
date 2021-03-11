ce440e13251749a20c959541c173da6b4417ad13
Kriyo <krikenn@gmail.com>
2021-03-11 15:54:06 +0000

    Added reducer function for calculating balance and added it to context

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import {
   Card,
@@ -8,19 +8,21 @@ import {
   Grid,
   Divider,
 } from '@material-ui/core'
+import { ExpenseTrackerContext } from '../../context/context'
 import { Form } from './Form/Form'
 import { List } from './List/List'
 import useStyles from './styles'
 
 export const Main = () => {
   const classes = useStyles()
+  const { balance } = useContext(ExpenseTrackerContext)
 
   return (
     <Card className={classes.root}>
       <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
       <CardContent>
         <Typography align="center" variant="h5">
-          Total Balance €100
+          Total Balance €{balance}
         </Typography>
         <Typography
           variant="subtitle1"
